75eb5b3d50b1fe8465029eec5d3b339e64d4c0c1
emmanuelishola123 <emmanuelishola7@gmail.com>
2024-04-08 18:02:28 +0100

    fix build-error

--- webapp/src/redux/reducer/socketReducer.ts
+++ webapp/src/redux/reducer/socketReducer.ts
@@ -1,29 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { socketState } from "../../@types";
+// import { createSlice } from "@reduxjs/toolkit";
+// import { socketState } from "../../@types";
 
 
-const initialState: socketState = {
-    isConnected: false,
-    socket: null,
-    socketId: null,
-};
+// const initialState: socketState = {
+//     isConnected: false,
+//     socket: null,
+//     socketId: null,
+// };
 
-const socketSlice = createSlice({
-    name: "socket",
-    initialState,
-    reducers: {
-        connectSocket: (state, action) => {
-            state = {
-                isConnected: true,
-                socket: action.payload.socket,
-                socketId: action.payload.socketId
-            }
-        },
-        disconnectSocket: (state) => {
-            state = { isConnected: false, socketId: null, socket: null }
-        },
-    },
-});
+// const socketSlice = createSlice({
+//     name: "socket",
+//     initialState,
+//     reducers: {
+//         connectSocket: (state, action) => {
+//             state = {
+//                 isConnected: true,
+//                 socket: action.payload.socket,
+//                 socketId: action.payload.socketId
+//             }
+//         },
+//         disconnectSocket: (state) => {
+//             state = { isConnected: false, socketId: null, socket: null }
+//         },
+//     },
+// });
 
-export const { connectSocket, disconnectSocket } = socketSlice.actions;
-export default socketSlice.reducer;
+// export const { connectSocket, disconnectSocket } = socketSlice.actions;
+// export default socketSlice.reducer;
--- webapp/src/redux/store.ts
+++ webapp/src/redux/store.ts
@@ -1,13 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducer/userReducer";
-import socketReducer from "./reducer/socketReducer";
+// import socketReducer from "./reducer/socketReducer";
 import callReducer from "./reducer/callReducer";
 import layoutReducer from './reducer/layoutReducer'
 
 const store = configureStore({
   reducer: {
     userReducer: userReducer,
-    socketReducer: socketReducer,
+    // socketReducer: socketReducer,
     callReducer: callReducer,
     layoutReducer: layoutReducer
   },
